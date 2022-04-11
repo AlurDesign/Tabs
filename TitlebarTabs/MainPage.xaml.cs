4f3b1e7c6fe963d58fc78e8dd0152170ad08dd42
AlurDesign <93062699+AlurDesign@users.noreply.github.com>
2022-04-11 16:08:17 +0200

    Code

---
+++ TitlebarTabs/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace TitlebarTabs
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Hide default title bar.
+            CoreApplicationViewTitleBar coreTitleBar =
+                CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            // Set caption buttons background to transparent.
+            ApplicationViewTitleBar titleBar =
+                ApplicationView.GetForCurrentView().TitleBar;
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+
+            // Set XAML element as a drag region.
+            Window.Current.SetTitleBar(AppTitleBar);
+
+            // Register a handler for when the title bar visibility changes.
+            // For example, when the title bar is invoked in full screen mode.
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+
+            // Register a handler for when the window activation changes.
+            Window.Current.CoreWindow.Activated += CoreWindow_Activated;
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (sender.IsVisible)
+            {
+                AppTitleBar.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                AppTitleBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void CoreWindow_Activated(CoreWindow sender, WindowActivatedEventArgs args)
+        {
+            UISettings settings = new UISettings();
+            if (args.WindowActivationState == CoreWindowActivationState.Deactivated)
+            {
+                //App is in the background
+            }
+            else
+            {
+                //App is focused
+            }
+        }
+
+        //TabView related things
+        private void TabView_Loaded(object sender, RoutedEventArgs e)
+        {
+            (sender as TabView).TabItems.Add(CreateNewTab(1));
+        }
+
+        private void TabView_AddButtonClick(TabView sender, object args)
+        {
+            sender.TabItems.Add(CreateNewTab(sender.TabItems.Count));
+        }
+
+        private void TabView_TabCloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args)
+        {
+            sender.TabItems.Remove(args.Tab);
+        }
+
+        private TabViewItem CreateNewTab(int index)
+        {
+            TabViewItem newItem = new TabViewItem();
+
+            newItem.Header = $"Document {index}";
+            newItem.IconSource = new Microsoft.UI.Xaml.Controls.SymbolIconSource() { Symbol = Symbol.Document };
+            Frame frame = new Frame();
+            frame.Navigate(typeof(TabContent));
+
+            newItem.Content = frame;
+
+            return newItem;
+        }
+    }
+}
